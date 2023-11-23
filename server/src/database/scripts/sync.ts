11d59bd42eadb41e69a97ebdb11d1e4cf8014b68
Bridge-PC <giovanespace@gmail.com>
2023-11-23 14:32:00 -0300

    Espero que tenha finalmente resolvido o bug do sync

--- server/src/database/scripts/sync.ts
+++ server/src/database/scripts/sync.ts
@@ -2,18 +2,18 @@ import User from "../models/User"
 import Note from "../models/Note";
 import UserNote from '../models/UserNote'
 
-try {
-User.sync({ force: true }).then(() => {
-  console.log('Tabela "users" criada com sucesso!')
-}).catch(error => console.error('Erro ao criar tabela "users":', error))
+const createTables = async () => {
+  await User.sync({ force: true }).then(() => {
+    console.log('Tabela "users" criada com sucesso!')
+  }).catch(error => console.error('Erro ao criar tabela "users":', error))
 
-Note.sync({ force: true }).then(() => {
-  console.log('Tabela "notes" criada com sucesso!')
-}).catch(error => console.error('Erro ao criar tabela "notes":', error))
-} catch (error) {
-  console.error(error)
+  await Note.sync({ force: true }).then(() => {
+    console.log('Tabela "notes" criada com sucesso!')
+  }).catch(error => console.error('Erro ao criar tabela "notes":', error))
+
+  await UserNote.sync({ force: true }).then(() => {
+    console.log('Tabela "user_notes" criada com sucesso!')
+}).catch(error => console.error('Erro ao criar tabela "user_notes":', error))
 }
 
-UserNote.sync({ force: true }).then(() => {
-  console.log('Tabela "user_notes" criada com sucesso!')
-}).catch(error => console.error('Erro ao criar tabela "user_notes":', error))
+createTables()
