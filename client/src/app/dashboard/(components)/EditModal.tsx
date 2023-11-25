d943bf68dd889c7bf4aa95f56d9c439a8bc7479e
GuiHenriques <gui10ghc@gmail.com>
2023-11-25 18:37:17 -0300

    edit modal front

---
+++ client/src/app/dashboard/(components)/EditModal.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import Button from "../../(components)/Button";
+interface ModalProps {
+	isOpen: boolean;
+	onClose: () => void;
+	note: { titulo: string; texto: string };
+	onSave: (updatedNote: { titulo: string; texto: string }) => void;
+}
+
+const Modal: React.FC<ModalProps> = ({ isOpen, onClose, note, onSave }) => {
+	const [titulo, setTitulo] = React.useState(note.titulo);
+	const [texto, setTexto] = React.useState(note.texto);
+
+	const handleSave = () => {
+		onSave({ titulo, texto });
+		onClose();
+	};
+
+	if (!isOpen) return null;
+
+	return (
+		<div className="fixed inset-0 bg-gray-600 bg-opacity-50">
+			<div className="relative top-20 mx-auto p-5 border md:w-1/3 w-3/4 shadow-lg rounded-md bg-white">
+				<div className="mt-3 text-center">
+					<h3 className="text-lg leading-6 font-medium text-gray-900">
+						Editar Nota
+					</h3>
+					<div className="flex flex-col justify-center items-center m-4">
+						<input
+							type="text"
+							className="input1 w-full p-2" // Adjusted for full width
+							value={titulo}
+							onChange={(e) => setTitulo(e.target.value)}
+						/>
+						<textarea
+							className="input1 w-full p-2 mt-2" // Adjusted for full width
+							rows={5}
+							value={texto}
+							onChange={(e) => setTexto(e.target.value)}
+						/>
+					</div>
+
+					<div className="flex justify-center space-x-4">
+						<Button text="Cancel" onClick={onClose} />
+						<Button text="Save" onClick={handleSave} />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Modal;
--- client/src/app/dashboard/(components)/Note.tsx
+++ client/src/app/dashboard/(components)/Note.tsx
@@ -1,5 +1,6 @@
 import React, { useState} from "react";
 import NoteIcon from "./NoteIcon";
+import EditModal from "./EditModal";
 
 type NoteProps = {
 	note: any;
@@ -7,6 +8,17 @@ type NoteProps = {
 };
 
 export default function Note(props: NoteProps) {
+	
+	const [isModalOpen, setIsModalOpen] = useState(false);
+
+	const openEditModal = () => setIsModalOpen(true);
+	const closeModal = () => setIsModalOpen(false);
+
+	const handleSave = (updatedNote: { titulo: string; texto: string }) => {
+		// Update the note here
+		closeModal();
+	};
+	
 	// State to track visibility of icons
 	const [showIcons, setShowIcons] = useState(false);
 
@@ -16,34 +28,48 @@ export default function Note(props: NoteProps) {
 	};
 
 	return (
-		<tr className="bg-white hover:bg-gray-100 group" key={props.note.id}>
-			<td className="px-4 py-2 w-1/4">{props.note.titulo}</td>
-			<td className="px-4 py-2 border-2 w-3/4 border-red-200">
-				{props.note.texto}
-			</td>
+		<>
+			<EditModal
+				isOpen={isModalOpen}
+				onClose={closeModal}
+				note={{ titulo: props.note.titulo, texto: props.note.texto }}
+				onSave={handleSave}
+			/>
+			<tr
+				className="bg-white hover:bg-gray-100 group"
+				key={props.note.id}
+			>
+				<td className="px-4 py-2 w-1/4">{props.note.titulo}</td>
+				<td className="px-4 py-2 border-2 w-3/4 border-red-200">
+					{props.note.texto}
+				</td>
 
-			<td className="py-2 relative">
-				<div className="md:flex hidden items-center space-x-2 opacity-0 group-hover:opacity-100">
-					<NoteIcon name="edit" />
-					<NoteIcon name="person_add" />
-					<NoteIcon name="delete" onClick={props.onClick} />
-				</div>
-				
-				<div className="md:hidden" onClick={toggleIcons}>
-					<NoteIcon name="more_vert" />
-				</div>
-				
-				{showIcons && ( // Conditional rendering based on showIcons state
-					<div className="absolute right-0 mt-2 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
-						<div className="">
-							<NoteIcon name="edit" />
-							<NoteIcon name="person_add" />
-							<NoteIcon name="delete" onClick={props.onClick} />
-						</div>
+				<td className="py-2 relative">
+					<div className="md:flex hidden items-center space-x-2 opacity-0 group-hover:opacity-100">
+						<NoteIcon name="edit" onClick={openEditModal} />
+						<NoteIcon name="person_add" />
+						<NoteIcon name="delete" onClick={props.onClick} />
 					</div>
-				)}
-			</td>
-		</tr>
+
+					<div className="md:hidden" onClick={toggleIcons}>
+						<NoteIcon name="more_vert" />
+					</div>
+
+					{showIcons && ( // Conditional rendering based on showIcons state
+						<div className="relative right-0 mt-2 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+							<div className="">
+								<NoteIcon name="edit" onClick={openEditModal} />
+								<NoteIcon name="person_add" />
+								<NoteIcon
+									name="delete"
+									onClick={props.onClick}
+								/>
+							</div>
+						</div>
+					)}
+				</td>
+			</tr>
+		</>
 	);
 }
 
