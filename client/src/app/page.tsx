31457005dc1d8695ef0a22bf5c28a8428485a075
Isabela Vill de Aquino <110916619+isabelavill@users.noreply.github.com>
2023-11-27 10:22:40 -0300

    doc app

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation"
 
-export default function RootPage() {
+export default function RootPage() { /*função de exportação padrão do módulo*/
   redirect('/home')
-}
+}/*Não renderiza nenhum conteúdo visual, redireciona ('redirect') o usuário para a página home*/
