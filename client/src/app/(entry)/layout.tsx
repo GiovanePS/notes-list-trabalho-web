c0ffc02e7a535bdb7c8a5758ab7dfe0eabb5530a
Isabela Vill de Aquino <110916619+isabelavill@users.noreply.github.com>
2023-11-27 12:04:40 -0300

    doc entry

--- client/src/app/(entry)/layout.tsx
+++ client/src/app/(entry)/layout.tsx
@@ -5,12 +5,12 @@ export const metadata: Metadata = {
 	title: "Entrar",
 };
 
-export default function RootLayout({children}: {children: React.ReactNode}) {
+export default function RootLayout({children}: {children: React.ReactNode}) { /*Representa um layout base para a página de entrada ("Entrar").*/
 	return (
 		<>
 			<HomeNavBar />
 			<div className="min-h-screen flex flex-col justify-center items-center bg-white text-white mt-0.5">
-				<div className="w-full max-w-xs">{children}</div>
+				<div className="w-full max-w-xs">{children}</div> {/*renderiza um contêiner flexível Que cobre no mínimo a altura da tela centrado verticalmente e horizontalmente */}
 			</div>
 		</>
 	);
