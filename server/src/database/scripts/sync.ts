1c15b16c1c559227ccacfee6390a2d6154216a10
Isabela Vill de Aquino <110916619+isabelavill@users.noreply.github.com>
2023-11-28 01:41:34 -0300

    doc scripts

--- server/src/database/scripts/sync.ts
+++ server/src/database/scripts/sync.ts
@@ -1,17 +1,17 @@
 import User from "../models/User"
 import Note from "../models/Note";
 import UserNote from '../models/UserNote'
-
-const createTables = async () => {
+/*Código responsável por criar as tabelas no banco de dados usando o Sequelize*/
+const createTables = async () => {   // Cria a tabela "users"
   await User.sync({ force: true }).then(() => {
     console.log('Tabela "users" criada com sucesso!')
   }).catch((error: Error) => console.error('Erro ao criar tabela "users":', error))
 
-  await Note.sync({ force: true }).then(() => {
+  await Note.sync({ force: true }).then(() => {   // Cria a tabela "notes"
     console.log('Tabela "notes" criada com sucesso!')
   }).catch((error: Error) => console.error('Erro ao criar tabela "notes":', error))
 
-  await UserNote.sync({ force: true }).then(() => {
+  await UserNote.sync({ force: true }).then(() => {  // Cria a tabela "user_notes"
     console.log('Tabela "user_notes" criada com sucesso!')
   }).catch((error: Error) => console.error('Erro ao criar tabela "user_notes":', error))
 }
