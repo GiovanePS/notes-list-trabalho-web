69f1b2132e2a4bf41206816e57c6cf8c4871561e
Bridge-PC <giovanespace@gmail.com>
2023-11-28 17:32:20 -0300

    tentando qualquer coisa

--- server/src/server.ts
+++ server/src/server.ts
@@ -17,7 +17,8 @@ app.use(express.json()) /*Usado para analisar o corpo das solicitações como JS
 app.use(cors({
   origin: 'https://notes-list-client.vercel.app',
   credentials: true,
-  exposedHeaders: ['Content-Type', 'Set-Cookie']
+  exposedHeaders: ['Content-Type', 'Set-Cookie'],
+  allowedHeaders: ['Content-Type', 'Set-Cookie'],
 }))
 
 app.use(session({
