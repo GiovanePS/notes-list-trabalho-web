f2fc381a86164ec69bf0a8d88df24f1d693043af
Isabela Vill de Aquino <110916619+isabelavill@users.noreply.github.com>
2023-11-28 01:28:41 -0300

    doc db

--- server/src/database/sequelize_module.ts
+++ server/src/database/sequelize_module.ts
@@ -1,9 +1,10 @@
 import { Sequelize } from 'sequelize'
 import 'dotenv/config'
 import path from 'path'
-const envPath = path.resolve(__dirname, '..', '.env')
-require('dotenv').config({ path: envPath })
+const envPath = path.resolve(__dirname, '..', '.env') /*Caminho para o arquivo .env. Este caminho é construído usando o módulo path para garantir que seja resolvido corretamente, mesmo se o script estiver em um diretório diferente.*/
+require('dotenv').config({ path: envPath }) /*Carrega as variáveis de ambiente do arquivo .env no caminho especificado.*/
 
+/*Configuração básica do Sequelize para uma conexão com um banco de dados PostgreSQL. */
 const sequelize = new Sequelize({
   dialect: 'postgres',
   host: 'localhost',
